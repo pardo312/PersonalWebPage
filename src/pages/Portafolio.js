3aa00e9c03af89e454d389092c8acb8d3bc47a2c
Jiufen <da.pardo@uniandes.edu.co>
2020-10-04 22:10:34 -0300

    Añadido proyectos

--- src/pages/Portafolio.js
+++ src/pages/Portafolio.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Slider from '../components/NetflixSlider'
 
 function Portafolio() {
+    const movies = [
+        {
+          id: 1,
+          image: '/images/BoomAwayBanner.jpeg',
+          imageBg: '/images/BoomAwayContent.png',
+          title: 'BoomAway'
+        },
+        {
+          id: 2,
+          image: '/images/SummerGameBanner.png',
+          imageBg: '/images/SummerGameContent.png',
+          title: 'SummerGame'
+        },
+        {
+          id: 3,
+          image: '/images/slide3.jpg',
+          imageBg: '/images/slide3b.webp',
+          title: 'The rain',
+        },
+        {
+          id: 4,
+          image: '/images/slide4.jpg',
+          imageBg: '/images/slide4b.webp',
+          title: 'Sex education'
+        }
+      ];
+      
     return (
         <div className="portafolio">
-            <h1>Portafolio</h1>
+            <div className="app">
+                <Slider>
+                {movies.map(movie => (
+                    <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+                ))}
+                </Slider>
+               </div>
         </div>
     )
 }
