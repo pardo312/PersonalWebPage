5abaf2a03b8757fda3b8e3cba2847bc8059519ca
Jiufen <da.pardo@uniandes.edu.co>
2021-03-30 11:05:57 -0500

    Beautify About Page

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from "react";
+import { IconContext } from "react-icons";
+import * as FaIcons from "react-icons/fa";
 
 function AboutMe() {
-    return (
-        <div className="aboutMe">
-            <div className="container" >
-                <h1 className="banner">Contacto </h1>
-                <div className="row" style={{marginTop:"50px"}} >
-                    <div class="col-xl-12 col-12">
-                        <p className="title">
-                            Correo: pardod312@gmail.com
-                        </p>
-                    </div>
-                </div>
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <div className="aboutMe">
+        <img className="leftBanner" src="https://images.freeimages.com/images/large-previews/eec/get-in-touch-1257696.jpg"></img>
+        <div className="container">
+          <h1 className="banner">Contacto </h1>
+          <div className="row" style={{ marginTop: "50px" }}>
+            <div class="col-xl-12 col-12">
+              <a href="mailto:pardod312@gmail.com" target="_blank">
+                <p className="title">
+                  <FaIcons.FaEnvelope />Correo: pardod312@gmail.com
+                </p>
+              </a>
+              <a href="https://www.instagram.com/jiufen_dp/" target="_blank">
+                <p className="title">
+                  <FaIcons.FaInstagram />Instagram: jiufen_dp
+                </p>
+              </a>
+              <a href="https://github.com/pardo312" target="_blank">
+                <p className="title">
+                  <FaIcons.FaGithub />Github: pardo312
+                </p>
+              </a>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </IconContext.Provider>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
