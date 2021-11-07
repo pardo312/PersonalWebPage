eb698dbdf1725f0cfadf11c47acf390ea5db53e3
Daniel <d.pardo@getaclub.io>
2021-11-07 13:16:57 -0500

    Fixes for deploy

---
+++ build/precache-manifest.c1011dc62ace8a3a83f142b1c60d07ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65360ef9118942315af8ff671ccf3c57",
+    "url": "/PersonalWebPage/index.html"
+  },
+  {
+    "revision": "a0f479ec5432dd7ac1f6",
+    "url": "/PersonalWebPage/static/css/main.61e3a577.chunk.css"
+  },
+  {
+    "revision": "078e9cc1c1997247c58d",
+    "url": "/PersonalWebPage/static/js/2.dcccd728.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PersonalWebPage/static/js/2.dcccd728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f479ec5432dd7ac1f6",
+    "url": "/PersonalWebPage/static/js/main.b7ee10c0.chunk.js"
+  },
+  {
+    "revision": "3b68b8bd4354016dc6a9",
+    "url": "/PersonalWebPage/static/js/runtime-main.9b2b9c2d.js"
+  }
+]);
